10abdbc5b41834e8ff9c02f5f7b4b196d7aa0513
Andrey Pushkarev <fealaer@gmail.com>
2014-12-30 19:10:19 +0530

    Added ingredients to drink details

--- www/app/modules/tabs/drinks/item.controller.js
+++ www/app/modules/tabs/drinks/item.controller.js
@@ -1,6 +1,11 @@
 'use strict';
 
 angular.module('avm.tabs.drinks')
-	.controller('DrinksItemCtrl', function ($scope, item) {
+	.controller('DrinksItemCtrl', function ($scope, item, ingredients) {
 		$scope.item = item;
+		$scope.ingredients = ingredients;
+
+		$scope.usedIngredients = function (ingredient) {
+			return _.contains($scope.item.ingredients, ingredient.id);
+		}
 	});
