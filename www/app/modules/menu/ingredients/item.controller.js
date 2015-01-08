68d2bc28208255eefbdcc32f2bcfca403112586b
Andrey Pushkarev <fealaer@gmail.com>
2015-01-08 21:31:23 +0530

    Added Wikipedia button on ingredient details page

--- www/app/modules/menu/ingredients/item.controller.js
+++ www/app/modules/menu/ingredients/item.controller.js
@@ -1,6 +1,17 @@
 'use strict';
 
 angular.module('avm.menu.ingredients')
-	.controller('IngredientsItemCtrl', function ($scope, item) {
+	.controller('IngredientsItemCtrl', function ($rootScope, $scope, item) {
 		$scope.item = item;
+    var lang = $rootScope.lang;
+
+    $scope.wiki = function (wiki) {
+      var url;
+      if (wiki[lang]) {
+        url = wiki[lang];
+      } else {
+        url = wiki.en;
+      }
+      window.open(url, '_system');
+    }
 	});
