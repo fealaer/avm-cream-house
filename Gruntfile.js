85b1022ecf041b2b6b4181c40f8a10e191d929d1
Andrey Pushkarev <fealaer@gmail.com>
2015-01-13 18:41:51 +0530

    Added grunt tasks compile and extract for  angular-gettext (translation support)

--- Gruntfile.js
+++ Gruntfile.js
@@ -8,7 +8,25 @@ Licensed under the MIT license.
 
 module.exports = function(grunt) {
   grunt.loadNpmTasks("grunt-steroids");
+  grunt.loadNpmTasks('grunt-angular-gettext');
   grunt.registerTask("default", [
     "steroids-make-fresh"
   ]);
-}
+
+  grunt.initConfig({
+    nggettext_extract: {
+      pot: {
+        files: {
+          'po/en.pot': ['www/**/*.html', 'www/app/**/*.js']
+        }
+      }
+    },
+    nggettext_compile: {
+      all: {
+        files: {
+          'www/app/components/translations.js': ['po/*.po']
+        }
+      }
+    }
+  })
+};
