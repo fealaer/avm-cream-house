f42d03abac00569be97c1485ba08885a14cb26a3
Andrey Pushkarev <fealaer@gmail.com>
2015-02-17 21:54:18 +0530

    Optimized size of  images

--- www/app/components/libs.js
+++ www/app/components/libs.js
@@ -9,3 +9,21 @@ angular.module('avm.libs', [
   'gettext',
   'angular-md5'
 ]);
+/*
+
+ "ionic": "~0.9.27",
+ "lodash": "~3.2.0",
+ "restangular": "~1.4.0",
+ "ngstorage": "~0.3.0",
+ "font-awesome": "~4.3.0",
+ "angular-fontawesome": "~0.3.0",
+ "fastclick": "~1.0.6",
+ "angular": "~1.3.13",
+ "angular-animate": "~1.3.13",
+ "angular-aria": "~1.3.13",
+ "angular-sanitize": "~1.3.13",
+ "angular-ui-router": "~0.2.13",
+ "ionicons": "~2.0.1",
+ "angular-gettext": "~2.0.2",
+ "angular-md5": "~0.1.7"
+ */
