f4cdeab983540dadea48a5cebbd85cc7c5a856e3
Andrey Pushkarev <fealaer@gmail.com>
2015-02-17 22:52:41 +0530

    Added cleaned bower dependencies to repository

--- www/app/components/libs.js
+++ www/app/components/libs.js
@@ -10,7 +10,8 @@ angular.module('avm.libs', [
   'angular-md5'
 ]);
 /*
-
+ "supersonic": "~1.1.1",
+ "webcomponentsjs": "~0.5.4",
  "ionic": "~0.9.27",
  "lodash": "~3.2.0",
  "restangular": "~1.4.0",
