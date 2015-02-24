14f7349d50c54022d639ac14ed791159bc0f4c4d
Andrey Pushkarev <fealaer@gmail.com>
2015-02-24 22:24:07 +0530

    Fixed ordering: use reverse order of lists when they sorted by rating

--- www/app/modules/menu/drinks/list.controller.js
+++ www/app/modules/menu/drinks/list.controller.js
@@ -4,6 +4,8 @@ angular.module('avm.menu.drinks')
 	.controller('DrinksListCtrl', function ($scope, $filter, $state, items, listFilter, $ionicScrollDelegate) {
 		$scope.items = items;
     $scope.filtered;
+    $scope.reverse;
+
 		$scope.$on('filterChanged', function(event, listName) {
       if (listName === 'drinks') {
         setFilter();
@@ -13,6 +15,7 @@ angular.module('avm.menu.drinks')
 		function setFilter() {
 			$scope.search = angular.copy(listFilter.get('drinks'));
       $scope.filtered = $filter('filter')($scope.items, $scope.search.filter).length;
+      $scope.reverse = ($scope.search.order.by === 'rate.rate' ? !$scope.search.order.reverse : $scope.search.order.reverse)
 		}
 
 		$scope.$on('$ionicView.afterEnter', function() {
--- www/app/modules/menu/ingredients/list.controller.js
+++ www/app/modules/menu/ingredients/list.controller.js
@@ -4,6 +4,8 @@ angular.module('avm.menu.ingredients')
 	.controller('IngredientsListCtrl', function ($scope, $filter, $state, ingredients, listFilter, $ionicScrollDelegate) {
 		$scope.items = ingredients;
     $scope.filtered;
+    $scope.reverse
+
 		$scope.$on('filterChanged', function(event, listName) {
       if (listName === 'ingredients') {
         setFilter();
@@ -13,6 +15,7 @@ angular.module('avm.menu.ingredients')
 		function setFilter() {
 			$scope.search = angular.copy(listFilter.get('ingredients'));
       $scope.filtered = $filter('filter')($scope.items, $scope.search.filter).length;
+      $scope.reverse = ($scope.search.order.by === 'rate.rate' ? !$scope.search.order.reverse : $scope.search.order.reverse)
 		}
 
     $scope.$on('$ionicView.afterEnter', function() {
