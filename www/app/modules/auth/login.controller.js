920138659803e44ab78ede36f7398a26710a861e
Andrey Pushkarev <fealaer@gmail.com>
2015-03-02 17:52:37 +0530

    Added form validation for login

--- www/app/modules/auth/login.controller.js
+++ www/app/modules/auth/login.controller.js
@@ -2,7 +2,7 @@
 
 angular.module('avm.auth')
 
-  .controller('LoginCtrl', function ($scope, account, $state, gettextCatalog, $timeout) {
+  .controller('LoginCtrl', function ($scope, account, $state, gettextCatalog, $timeout, toastService) {
     var defaultData = {
       email: '',
       password: ''
@@ -12,13 +12,28 @@ angular.module('avm.auth')
 
     $scope.errorMessages = [];
 
-    $scope.login = function () {
-      account.login($scope.data)
-        .then(function (response) {
-          $timeout(function(){
-            $state.go('menu.drinks');
-            $scope.data = angular.copy(defaultData);
+    $scope.login = function (form) {
+      if (form.$valid) {
+        account.login($scope.data)
+          .then(function (response) {
+            $timeout(function () {
+              $state.go('menu.drinks');
+              $scope.data = angular.copy(defaultData);
+            });
           });
-        });
+      } else {
+        if (form.email.$error.required) {
+          toastService.showLongCenter(gettextCatalog.getString('Email is required.'));
+        }
+        if (form.email.$error.email) {
+          toastService.showLongCenter(gettextCatalog.getString('Email is not valid.'));
+        }
+        if (form.password.$error.required) {
+          toastService.showLongCenter(gettextCatalog.getString('Password is required.'));
+        }
+        if (form.password.$error.minlength) {
+          toastService.showLongCenter(gettextCatalog.getString('Password must be at least 4 characters long.'));
+        }
+      }
     };
   });
